29d58f7994a84f82e04baf253ae55640b41ef674
Xiangpeng Yang <knightyxp@gmail.com>
2025-11-08 03:41:58 +1100

    no cpu off load

--- test.sh
+++ test.sh
@@ -8,6 +8,5 @@ python sample_video.py \
     --cfg-scale 6.0 \
     --flow-shift 7.0 \
     --flow-reverse \
-    --use-cpu-offload \
     --save-path ./hunyuan_results \
     --video /scratch3/yan204/yxp/VideoX_Fun/data/test_json/4tasks_rem_add_swap_local-style_test.json

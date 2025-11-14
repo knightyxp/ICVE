aa54856657a65f23c5773de470a304bab1d3326e
Xiangpeng Yang <knightyxp@gmail.com>
2025-11-14 13:11:40 +1100

    161 frames

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 python sample_video.py \
     --dit-weight checkpoint/diffusion_pytorch_model.safetensors \
-    --video-length 33 \
+    --video-length 161 \
     --infer-steps 50 \
     --seed 42 \
     --embedded-cfg-scale 1.0 \
     --cfg-scale 6.0 \
     --flow-shift 7.0 \
     --flow-reverse \
-    --save-path ./long_video_results \
+    --save-path ./long_video_results_161_frames \
     --video /scratch3/yan204/yxp/VideoX_Fun/data/test_json/long_video_new.json 
